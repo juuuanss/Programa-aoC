af8c07c7fd7ffaf0408cabb6286c18f8dd3a03d5
juuuanss <gabrielljuan45@gmail.com>
2025-09-09 01:07:56 +0000

    PRINT F DE LEITURA  DE CARTA01

--- SUPER TRUNFO EM C.c
+++ SUPER TRUNFO EM C.c
@@ -11,11 +11,34 @@ int main(){
     float PIB;
     int PONTOSTURISTICOS;
 
+printf("BEM VINDO AO JOGO \n");
 
 
+printf("ESTADO( Uma letra entre A a H)\n ");
+scanf("Estado: %s", ESTADO);
 
+printf("CODIGO (Letra do Estado seguida de  um numeri de 01 a 04)\n");
+scanf("CODIGO:%s", CODIGO);
 
+printf("CIDADE:\n");
+scanf("CIDADE:%s", CIDADE);
 
+printf("POPULAÇAO: \n");
+scanf("POPULAÇAO: %d", POPULACAO);
+
+printf("ÁREA( Em KM²)\n");
+scanf("AREA(Em KM²): %f", AREA);
+
+printf("PIB: /n");
+scanf("PIB: %f", PIB );
+
+printf("PONTOS TURISTICOS:\n");
+scanf("PONTOS TURISTICOS: %d", PONTOSTURISTICOS);
+
+printf("CARTA 01\n");
+
+printf(:%s \n");
+print("");
 
 
 
